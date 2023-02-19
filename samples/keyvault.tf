271ccdee2122ee215ba56641e63862150853914a
Pinaki Kundu <pinaki.kundu@ses.com>
2023-02-19 14:05:25 +0530

    - checkov suggestions

--- samples/keyvault.tf
+++ samples/keyvault.tf
@@ -5,7 +5,11 @@ resource "azurerm_key_vault" "this" {
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+  purge_protection_enabled    = true
+  sku_name                    = "standard"
 
-  sku_name = "standard"
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+  }
 }
